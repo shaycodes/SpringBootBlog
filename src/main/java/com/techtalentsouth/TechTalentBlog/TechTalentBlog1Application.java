bc108adfc11125811af5e882f8e51536ea9e08f0
Shay Lindsey <shaycodes@gmail.com>
2022-11-21 15:25:42 -0800

    Add files via upload

---
+++ src/main/java/com/techtalentsouth/TechTalentBlog/TechTalentBlog1Application.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.TechTalentBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalentBlog1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalentBlog1Application.class, args);
+	}
+
+}
